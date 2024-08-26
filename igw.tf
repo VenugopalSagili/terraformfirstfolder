6492b86b79150243dd433f20ae557a35ae4c8530
Venugopal Sagili <gopal.py321@gmail.com>
2024-08-26 15:47:34 +0530

    first modified terraform resoures

---
+++ igw.tf
@@ -0,0 +1,7 @@
+# Internet Gatway Creation
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+	tags = {
+        Name = "${var.IGW_name}"
+    }
+}
---
+++ vpc.tf
@@ -0,0 +1,10 @@
+# VPC Creation
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "${var.vpc_name}"
+	Owner = "Venugopal"
+	environment = "${var.environment}"
+    }
+}
